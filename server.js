c8b7c404690275174e6729088ca7fdf69a41a578
entropy <alexandrisanas@gmail.com>
2018-09-04 00:11:22 +0300

    deploy rect 1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const nodePort = process.env.PORT || 5000;
 
 
@@ -8,4 +9,16 @@ require('./api/index.js')(app);
 app.get('/', (req, res) => {
     res.send("hi there");
 })
+
+if (process.env.NODE_ENV === 'production') {
+    // looks for the asset inside build
+    app.use(express.static('client/build'));
+
+    // catch all case
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+
+}
+
 app.listen(nodePort, () => console.log(`running on port: ${nodePort}` ));
